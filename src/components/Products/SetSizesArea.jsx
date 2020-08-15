7c3a5f7f811db6be918f929683eb800680709f80
deatiger <deatiger1492@gmail.com>
2020-08-15 11:17:49 +0900

    Modified Hooks from useMemo to useEffect in SetSIzeArea.jsx

--- src/components/Products/SetSizesArea.jsx
+++ src/components/Products/SetSizesArea.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {TextInput} from "../UIkit";
 import IconButton from "@material-ui/core/IconButton";
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
@@ -71,7 +71,7 @@ const SetSizesArea = (props) => {
         props.setSizes(newSizes);
     }
 
-    const memoIndex = useMemo(() => {
+    useEffect(() => {
         setIndex(props.sizes.length)
     },[props.sizes.length])
 
